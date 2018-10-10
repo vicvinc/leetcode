67284d1d3089024edbc8afd8e2817a2c92221932
maxm <vicvinvinvinc@gmail.com>
2018-10-10 19:03:34 +0800

    [S] #2

--- 2-Add-Two-Numbers/index.js
+++ 2-Add-Two-Numbers/index.js
@@ -8,14 +8,10 @@ const twoSum = (m, n) => {
 
   const b = n.reverse();
 
-  console.info(a, b);
-
   let carry = 0;
 
   const largerArray = a.length >= b.length ? a : b;
   const smallerArray = a.length >= b.length ? b : a;
-
-  // console.info(largerArray, smallerArray);
 
   return largerArray.reduce((acc, cur, idx) => {
     if (idx < smallerArray.length) {
@@ -34,7 +30,69 @@ const twoSum = (m, n) => {
   }, []);
 };
 
-const a = "1234567890987654321".split("");
-const b = "8765432109012345678".split("");
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} l1
+ * @param {ListNode} l2
+ * @return {ListNode}
+ */
+function ListNode(val) {
+  this.val = val;
+  this.next = null;
+}
 
-console.log(solution(a, b));
+var addTwoNumbers = function(list1, list2) {
+  let l1 = list1;
+  let l2 = list2;
+
+  const head = new ListNode(0);
+  let current = head;
+
+  head.next = current;
+
+  let carry = 0;
+
+  while (l1 || l2) {
+    let curNode = new ListNode(0);
+    let sum = 0;
+    if (l1 && l2) {
+      sum = l1.val + l2.val + carry;
+    } else if (l1) {
+      sum = l1.val + carry;
+    } else if (l2) {
+      sum = l2.val + carry;
+    }
+    if (sum >= 10) {
+      carry = 1;
+      curNode.val = sum % 10;
+    } else {
+      carry = 0;
+      curNode.val = sum;
+    }
+    current.next = curNode;
+    current = current.next;
+    l1 && (l1 = l1.next);
+    l2 && (l2 = l2.next);
+  }
+  if (carry) {
+    current.next = new ListNode(1);
+  }
+  return head.next;
+};
+
+const head1 = new ListNode(1);
+let cur = new ListNode(8);
+head1.next = cur;
+
+const head2 = new ListNode(0);
+// cur = new ListNode(2);
+// cur.next = new ListNode(3);
+// head2.next = cur;
+
+console.log(addTwoNumbers(head1, head2));
