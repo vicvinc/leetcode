d73d312a4e03b8ac955b1097b9da9e05339b1291
maxm <vicvinvinvinc@gmail.com>
2018-10-11 10:31:50 +0800

    [S] #3

--- 3-Longest-Substring-Without-Repeating-Characters/index.js
+++ 3-Longest-Substring-Without-Repeating-Characters/index.js
@@ -3,5 +3,19 @@
  * @return {number}
  */
 var lengthOfLongestSubstring = function(s) {
-  const dp = [];
+  let dp = [];
+  let max = 0;
+  for (let i = 0; i < s.length; i++) {
+    let idx = dp.indexOf(s[i]);
+    if (idx > -1) {
+      const popStr = dp.slice(0, idx + 1);
+      dp = dp.slice(idx + 1, dp.length);
+    }
+    dp.push(s[i]);
+    max = Math.max(dp.length, max);
+  }
+  return max;
 };
+
+const t = "abcabcbb";
+console.log(lengthOfLongestSubstring(t));
