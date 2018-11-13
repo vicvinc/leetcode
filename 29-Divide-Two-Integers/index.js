570dffd1cea17c0555b47c44bcd61a313d5f8935
vicvinc <vicvinvinc@gmail.com>
2018-11-14 01:21:09 +0800

    fk: #29

--- 29-Divide-Two-Integers/index.js
+++ 29-Divide-Two-Integers/index.js
@@ -12,49 +12,54 @@ var divide = function(dividend, divisor) {
   const MAX_INT = MAX - 1;
   const MIN_INT = -MAX;
 
+  let dvd = Math.abs(dividend);
+  let dvs = Math.abs(divisor);
+
   if (dividend === MIN_INT && divisor === -1) return MAX_INT;
 
   let q = 0;
   let pos = (dividend > 0 && divisor > 0) || (dividend < 0 && divisor < 0);
-  let dvd = Math.abs(dividend);
-  let dvs = Math.abs(divisor);
+
   let count = 1;
 
   let sub = dvs;
   while (dvd >= dvs) {
-    // console.log("dvd, dvs, q", dvd, sub, q, count);
-    if (dvd >> 1 >= sub) {
-      sub <<= 1;
+    if (sub + sub < dvd) {
+      sub += sub;
       count <<= 1;
       dvd -= sub;
       q += count;
     } else {
-      sub >> dvs;
-      count >>= 1;
+      sub = dvs;
+      count = 1;
+      dvd -= dvs;
+      q++;
     }
-    // console.log("dvd, dvs, q", dvd, sub, q, count);
   }
-
+  q = pos ? q : -q;
   if (q > MAX_INT) return MAX_INT;
   if (q < MIN_INT) return MIN_INT;
-  return pos ? q : -q;
+  return q;
 };
 
 // console.log(divide(-2147483648, -1));
-// console.log(divide(-2147483648, 1));
+console.log(divide(-2147483648, 1));
 // console.log(divide(2147483647, -1));
 // console.log(divide(2147483647, 1));
-console.log(divide(-10, -3));
-console.log(divide(10, 3));
-console.log(divide(7, 3));
-console.log(divide(-29, 4));
+// console.log(divide(-10, -3));
+console.log(divide(10, 2));
+// console.log(divide(11, 1));
+// console.log(divide(-29, 4));
 
 // console.log(divide(1, 1));
 
 // console.log(divide(1, 10));
-
-for (let i = 0; i < 100; i += 10) {
-  for (let j = 0; j < 10; j++) {
-    console.log(i, j, divide(i, j));
+const test = () => {
+  for (let i = 10; i < 50; i++) {
+    for (let j = 0; j < 10; j++) {
+      console.log(-i, j, divide(-i, j));
+    }
   }
-}
+};
+
+// test();
