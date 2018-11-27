4459e3387dd43fdc1930fc5a2816981d3ddf354b
vicvinc <vicvinvinc@gmail.com>
2018-11-27 22:25:18 +0800

    solve: #55

--- 55-Can-jump/index.js
+++ 55-Can-jump/index.js
@@ -2,4 +2,20 @@
  * @param {number[]} nums
  * @return {boolean}
  */
-var canJump = function(nums) {};
+var canJump = function(nums) {
+  const nLen = nums.length;
+  let [idx, curStep, nextStep] = [0, 0, 0];
+  while (curStep - idx + 1 > 0) {
+    while (idx <= curStep && idx < nLen) {
+      nextStep = Math.max(nextStep, nums[idx] + idx);
+      if (idx >= nLen - 1) return true;
+      idx++;
+    }
+    curStep = nextStep;
+  }
+  return false;
+};
+
+const t = [3, 2, 1, 0, 4, 5, 6, 7, 8, 9];
+const t1 = [2, 3, 1, 1, 4];
+console.log(canJump(t1));
